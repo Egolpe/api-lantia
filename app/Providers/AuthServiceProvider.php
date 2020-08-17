6115413decca20d543772b6ef7ac2234bc59004f
Enrique <enriquegolpe@gmail.com>
2020-08-17 20:35:07 +0200

    corrigiendo error de login

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
-        Passport::personalAccessClientId('client-id');
+        Passport::personalAccessClientId(config('passport.personal_access_client.id'));
+        Passport::personalAccessClientSecret(config('passport.personal_access_client.secret'));
+        
         //
     }
 }
